d7eb8508259b73f45ac4d2a76d29b311308a8998
ggongjukim <kch7892003@gmail.com>
2023-01-07 23:56:56 +0900

    ✨ feat : detail api 진행중중

--- src/todo/components/list/index.tsx
+++ src/todo/components/list/index.tsx
@@ -3,17 +3,23 @@ import { ReactComponent as Add } from 'src/commons/assets/add.svg';
 import { AddDialog } from 'src/commons/dialog';
 import * as S from './styled';
 
+//   할일 목록
 export const ListItem = (props: any) => {
   const { title, content, updatedAt, id } = props.data;
   const date = updatedAt.split('T')[0];
+
+  const clickHandler = () => {
+    props.clickHandlerListItem(id);
+  };
   return (
-    <S.ListItem>
+    <S.ListItem onClick={clickHandler}>
       <div>{title}</div>
       <div>{date}</div>
     </S.ListItem>
   );
 };
 
+// 할일 추가 버튼
 export const ListAdd = () => {
   const [open, setOpen] = useState(false);
 
