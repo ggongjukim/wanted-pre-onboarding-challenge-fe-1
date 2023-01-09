a39d57a0a5aeebf5188b7c1f1e13a5a5c5d974ae
ggongjukim <kch7892003@gmail.com>
2023-01-09 20:58:41 +0900

    ✨ feat : 1차 기능 구현 완료

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import Todo from './todo/page';
 import Auth from './auth/page';
 
-const Router = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Todo />} />
-      <Route path="/auth" element={<Auth />} />
-    </Routes>
-  </BrowserRouter>
-);
+const checkToken = async () => {
+  const result = await localStorage.getItem('token');
+
+  console.log(`checkToken`, result);
+  return result;
+};
+const token = () => {
+  console.log(`호출`);
+  return localStorage.getItem('token');
+};
+const GoToDo = () => <>{token() ? <Todo /> : <Navigate to="/auth" />}</>;
+// <Route path="/" element={token() ? <Todo /> : <Navigate to="/auth" />} />
+
+const GoToAuth = () => <>{token() ? <Navigate to="/" /> : <Auth />}</>;
+
+const Router = () => {
+  console.log(`router`);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GoToDo />} />
+        <Route path="/auth" element={<GoToAuth />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
